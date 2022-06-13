2b8fb1cc0fc7cb32251665b10ee7fdf37ea6c989
Esther Camilla Akinyi <camillaodhiambo8@gmail.com>
2022-06-13 23:22:44 +0300

    Loopy loops

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+// //const names = (["Guadalupe","Ollie","Aki"], "surprise");
+
+function writeCards(names, event){
+    let cards = []
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return cards;
+} 
+writeCards(cards);
+
+let count = 10
+
+function countDown(count) {
+    while(count >= 0) {
+        console.log(count--)
+    }
+}
